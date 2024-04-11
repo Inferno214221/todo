c82d4034303df82fc99c7c772da6223dd9934899
Inferno214221 <inferno214221@gmail.com>
2024-04-11 11:03:40 +0930

    Create Regexes From Pattern Inputs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::{Arg, Command, ValueHint, builder::ArgAction, builder::ValueParser, builder::RangedU64ValueParser};
+use clap::{builder::{ArgAction, RangedU64ValueParser, ValueParser}, Arg, Command, ValueHint};
+use fancy_regex::Regex;
 use std::path::PathBuf;
 use std::collections::HashSet;
 use walkdir::{DirEntry, IntoIter, WalkDir};
@@ -6,6 +7,7 @@ use walkdir::{DirEntry, IntoIter, WalkDir};
 #[derive(Debug)]
 struct Args {
     paths: Vec<PathBuf>,
+    _patterns: Vec<Regex>,
     _output_file: Option<PathBuf>,
     _output_format: String,
     include_hidden: bool,
@@ -15,8 +17,10 @@ struct Args {
 
 fn main() {
     let args: Args = get_args();
-    // println!("{:?}", args);
-    //verification?
+    println!("{:?}", args);
+
+    //TODO: verification?
+
     let files: HashSet<PathBuf> = get_all_files(&args);
     println!("{:?}", files);
 }
@@ -28,19 +32,26 @@ fn get_args() -> Args {
         .about("A CLI program to find all instances of TODO notes within a file or directory")
         .disable_version_flag(true)
         .arg(
-            Arg::new("VERSION")
-                .help("TODO")
-                .short('v')
-                .long("version")
-                .action(ArgAction::Version)
-        )
-        .arg(
             Arg::new("PATH")
                 .help("TODO")
                 .action(ArgAction::Append)
                 .required(true)
                 .value_parser(ValueParser::path_buf())
                 .value_hint(ValueHint::AnyPath)
+        )
+        .arg(
+            Arg::new("STRING")
+                .help("TODO")
+                .short('s')
+                .long("string")
+                .action(ArgAction::Append)
+        )
+        .arg(
+            Arg::new("REGEX")
+                .help("TODO")
+                .short('r')
+                .long("regex")
+                .action(ArgAction::Append)
         )
         .arg(
             Arg::new("OUTPUT_FILE")
@@ -55,40 +66,60 @@ fn get_args() -> Args {
                 .help("TODO")
                 .short('f')
                 .long("format")
-                .default_value("")
+                .default_value("TODO")
                 .value_parser(ValueParser::string())
         )
         .arg(
             Arg::new("INCLUDE_HIDDEN")
-                .help("TODO")
+                .help("Include hidden files")
                 .short('a')
                 .long("hidden")
                 .action(ArgAction::SetTrue)
         )
         .arg(
             Arg::new("FOLLOW_LINKS")
-                .help("TODO")
+                .help("Follow symbolic links")
                 .short('l')
                 .long("follow")
                 .action(ArgAction::SetTrue)
         )
         .arg(
             Arg::new("MAX_DEPTH")
-                .help("TODO")
+                .help("Limit the depth of subdirectories included")
                 .short('d')
                 .long("depth")
                 .value_parser(RangedU64ValueParser::<usize>::new())
         )
+        .arg(
+            Arg::new("VERSION")
+                .help("Print version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::Version)
+        )
         .get_matches();
 
-    let mut paths: Vec<PathBuf> = Vec::new();
-    for path in matches.get_many::<PathBuf>("PATH").unwrap() {
-        paths.push(path.clone());
+    let mut patterns: Vec<Regex> = Vec::new();
+
+    if let Some(values) = matches.get_many::<String>("STRING") {
+        patterns.append(&mut values.cloned().filter_map(|value| {
+            let escaped_value: &str = &fancy_regex::escape(&value);
+            return Regex::new(escaped_value).ok();
+        }).collect::<Vec<Regex>>());
+    }
+
+    if let Some(values) = matches.get_many::<String>("REGEX") {
+        patterns.append(&mut values.cloned().filter_map(|value| {
+            return Regex::new(&value).ok();
+        }).collect::<Vec<Regex>>());
     }
 
     return Args {
-        paths,
+        // Unwrap because required value:
+        paths: matches.get_many::<PathBuf>("PATH").unwrap().cloned().collect::<Vec<PathBuf>>(),
+        _patterns: patterns,
         _output_file: matches.get_one::<PathBuf>("OUTPUT_FILE").cloned(),
+        // Unwrap because default value:
         _output_format: matches.get_one::<String>("OUTPUT_FORMAT").cloned().unwrap(),
         include_hidden: matches.get_flag("INCLUDE_HIDDEN"),
         follow_links: matches.get_flag("FOLLOW_LINKS"),
